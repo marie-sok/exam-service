3192cfd0690fdf479cc3861099599ad31edbfc99
marie-sok <sarrahmcfarlend@icloud.com>
2025-08-03 08:43:22 +0300

    course work exam-service upd 4.0

--- src/test/java/org/skypro/exam_service/ExamServiceApplicationTests.java
+++ src/test/java/org/skypro/exam_service/ExamServiceApplicationTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-@SpringBootTest
-class ExamServiceApplicationTests {
+@SpringBootTest class ExamServiceApplicationTests {
 
     @Test
     void contextLoads() {
